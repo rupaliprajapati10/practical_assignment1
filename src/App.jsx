cf8c9b3e59f450a2cd0798be6e23938247d61ec0
Rupali Bahau Prajapati <rupaliprajapati63@gmail.com>
2026-03-05 21:41:47 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import MessageDisplay from "./components/setAa.jsx";
+import BackgroundChange from "./components/setAb.jsx";
+import ControlledForm from "./components/setAc.jsx";
+import ControlledForm1 from "./components/setAd.jsx";
+
+function setAa(){
+  return(
+    /*<MessageDisplay/>
+    <BackgroundChange/>
+    <ControlledForm/>*/
+    <ControlledForm1/>
+
+  );
+}
+
+export default setAa;
